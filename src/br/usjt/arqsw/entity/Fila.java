37865f81d9bb8a8df3125f1fbaed0a3ac6bc10f7
Alunos <Alunos@MOCLII0117.animaedu.intranet>
2018-04-20 17:42:08 -0300

    Quinta atividade

---
+++ src/br/usjt/arqsw/entity/Fila.java
@@ -0,0 +1,53 @@
+package br.usjt.arqsw.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+/**
+ * 
+ * @author Gabriela Vieira - 81610253 - SIN3AN-MCA1
+ */
+@Entity
+public class Fila implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="id_fila")
+	@NotNull(message="A fila não pode ser vazia")
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@NotNull
+	@Size(min=5, max=45, message="O nome da fila deve estar entre 5 e 45 caracteres.")
+	@Column(name="nm_fila")
+	private String nome;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	@Override
+	public String toString() {
+		return "Fila [id=" + id + ", nome=" + nome + "]";
+	}
+	
+}
